7f245c3d60f44f0f555fe85e6aa5b5506d72cec8
Henok Teklu <henokzion@gmail.com>
2021-11-10 22:11:01 +0300

    defn added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { getWord } from './services';
+import { dict, getWord } from './services';
 
 
 function App() {
-  useEffect(()=> {
-    getWord()
+
+  const [word, setWord] = useState({})
+  const [phonetic, setPhonetic] = useState("")
+  const [meaning, setMeaning] = useState({})
+
+  useEffect(() => {
+    setWord(getWord())
   }, [])
 
+  useEffect(() => {
+    dict(word.word_string).then(res => {
+      setMeaning(res?.data[0]?.meanings[0])
+      setPhonetic(res?.data[0].phonetic)
+      console.log(res?.data[0])
+    })
+  }, [word])
+
+  const fetchWord = () => {
+    setWord(getWord())
+  }
+
   return (
-    <div className="space-y-4 ">
-      <div className="block">dfd</div>
+    <div className="items-center justify-center flex flex-col">
+
+      <div className="w-full md:w-96 justify-center items-center bg-white shadow-lg rounded-lg flex flex-col mt-20">
+        <div className="w-full p-4 justify-start flex flex-col">
+          <h4 className="border-b-2 text-3xl" >{word.word_string}</h4>
+          <p className="my-4">\{phonetic}\ - <i>{meaning?.partOfSpeech}</i></p>
+
+          <div class="font-sans flex-row items-center justify-center bg-blue-darker w-full">
+            {
+              meaning.definitions.map(def => {
+                return (
+                  <div class="overflow-hidden bg-white rounded w-full shadow-lg  leading-normal">
+                    <div href="#" class="block group p-4 border-b">
+                      <p class="font-bold text-lg mb-1 text-black">{def.definition}</p>
+                      <p class="text-grey-darker mb-2"> <i>eg.</i> {def.example}</p>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+
+          <button value="button" className="my-4 px-4 py-2 text-white hover:bg-blue-700 bg-blue-500" onClick={fetchWord}>
+            Next
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,19 @@
 import words from "../data/words.json"
+import axios from "axios"
 
 export const getWord = () => {
-    console.log(words.vocab_overview.length)
+  const total_word_count = words.vocab_overview.length
+  return words.vocab_overview[getRandomInt(total_word_count)]
+}
+
+export const dict = async (word) => {
+  try {
+    return await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/de/${word}`)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
